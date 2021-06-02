58b12ff44f5471d75070afd50d9132fb00eecde6
TAdhamjon1996 <t.adhamjon@mail.ru>
2021-06-02 14:40:32 +0300

    Fist task Header
    Sahifaning header qismi

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+ const myElem = <div>
+     <Header></Header>
+    </div>
+    
+
+
+
+ReactDOM.render( myElem , document.getElementById('root'));
