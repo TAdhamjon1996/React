8be62bbd56f3c19b6ee54df544796a994f348a8a
TAdhamjon1996 <t.adhamjon@mail.ru>
2021-06-04 09:08:07 +0300

    Update Blog
    Blog page update

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -4,7 +4,7 @@ import Des from '../assets/designer.jpg';
 import Team from '../assets/team.jpg';
 import Prog from '../assets/prog.jpg';
 import Fram from '../assets/framework.jfif';
-import { Media, Container, Row, Col, ListGroupItem, Card } from 'react-bootstrap';
+import { Media, Container, Row, Col, ListGroup, Card } from 'react-bootstrap';
 
 export default class Blog extends Component {
     render(){
@@ -14,49 +14,49 @@ export default class Blog extends Component {
                     <Row>
                         <Col md='9' >
                             <Media className='m-5'>
-                                <img width={150} height={150} className='mr-3' src={Des}  />
+                                <img width={150} height={150} className='mr-3' src={Des} alt='Blog' />
                                 <Media.Body>
                                     <h5>Blog post</h5>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                 </Media.Body>
                             </Media>
                             <Media className='m-5'>
-                                <img width={150} height={150} className='mr-3' src={Des}  />
+                                <img width={150} height={150} className='mr-3' src={Team} alt='Blog' />
                                 <Media.Body>
                                     <h5>Blog post</h5>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                 </Media.Body>
                             </Media>
                             <Media className='m-5'>
-                                <img width={150} height={150} className='mr-3' src={Des}  />
+                                <img width={150} height={150} className='mr-3' src={Prog} alt='Blog' />
                                 <Media.Body>
                                     <h5>Blog post</h5>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                 </Media.Body>
                             </Media>
                             <Media className='m-5'>
-                                <img width={150} height={150} className='mr-3' src={Des}  />
+                                <img width={150} height={150} className='mr-3' src={Fram} alt='Blog' />
                                 <Media.Body>
                                     <h5>Blog post</h5>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                 </Media.Body>
                             </Media>
                         </Col>
-                        <Col md='3'>
-                            <h5>Category</h5>
+                        <Col md='3' >
+                            <h5 className='text-center mt-5'>Category</h5>
                             <Card>
-                                <List.Group>
+                                <ListGroup>
                                     <ListGroup.Item>Html/Css</ListGroup.Item>
                                     <ListGroup.Item>JavaScript</ListGroup.Item>
                                     <ListGroup.Item>Python</ListGroup.Item>
                                     <ListGroup.Item>Java</ListGroup.Item>
                                     <ListGroup.Item>C++</ListGroup.Item>
-                                </List.Group>
+                                </ListGroup>
                             </Card>
-                            <Card>
+                            <Card className='mt-3'>
                                 <Card.Body>
-                                    <Card.Title></Card.Title>
-                                    <Card.Text></Card.Text>
+                                    <Card.Title>Team Our</Card.Title>
+                                    <Card.Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
